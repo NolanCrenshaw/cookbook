a231736cbdc804d80c888dd1b08a5a2cfc4bcd1e
NolanCrenshaw <nolan.crenshaw@gmail.com>
2020-07-07 00:42:42 -0400

    rewrite of directory

--- express/app.js
+++ express/app.js
@@ -1,10 +1,3 @@
-/**
- * TODO: Create and configure your Express.js application in here.
- *       You must name the variable that contains your Express.js
- *       application "app" because that is what is exported at the
- *       bottom of the file.
- */
-
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
