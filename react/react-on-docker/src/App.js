945aed6fd7efcab4693e762e5130f88957127a6e
NolanCrenshaw <nolan.crenshaw@gmail.com>
2020-11-10 16:57:52 -0500

    build successful. tutorial complete final commit

--- react/react-on-docker/src/App.js
+++ react/react-on-docker/src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Hot Loading Works!
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
