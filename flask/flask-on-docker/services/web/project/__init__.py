0b31ce3f3c6410f17f3ff4d3f2fb08ca403694cd
NolanCrenshaw <nolan.crenshaw@gmail.com>
2020-11-10 13:45:46 -0500

    build working - final init commit

--- flask/flask-on-docker/services/web/project/__init__.py
+++ flask/flask-on-docker/services/web/project/__init__.py
@@ -1,4 +1,14 @@
-from flask import Flask, jsonify
+import os
+
+from werkzeug.utils import secure_filename
+from flask import (
+    Flask,
+    jsonify,
+    send_from_directory,
+    request,
+    redirect,
+    url_for
+)
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -21,3 +31,28 @@ class User(db.Model):
 @app.route("/")
 def hello_world():
     return jsonify(hello="world")
+
+
+@app.route("/static/<path:filename>")
+def staticfiles(filename):
+    return send_from_directory(app.config["STATIC_FOLDER"], filename)
+
+
+@app.route("/media/<path:filename>")
+def mediafiles(filename):
+    return send_from_directory(app.config["MEDIA_FOLDER"], filename)
+
+
+@app.route("/upload", methods=["GET", "POST"])
+def upload_file():
+    if request.method == "POST":
+        file = request.files["file"]
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config["MEDIA_FOLDER"], filename))
+    return f"""
+    <!doctype html>
+    <title>upload new File</title>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file><input type=submit value=Upload>
+    </form>
+    """
--- flask/flask-on-docker/services/web/project/config.py
+++ flask/flask-on-docker/services/web/project/config.py
@@ -8,3 +8,5 @@ class Config(object):
     # WHY? sqlite appears to be a default value. Yet it's a postgres/psql db.
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite://")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    STATIC_FOLDER = f"{os.getenv('APP_FOLDER')}/project/static"
+    MEDIA_FOLDER = f"{os.getenv('APP_FOLDER')}/project/media"
