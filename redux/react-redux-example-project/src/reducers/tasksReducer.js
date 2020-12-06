b014d9b3380bd8f2e2b628229681f02a7949ee82
NolanCrenshaw <nolan.crenshaw@gmail.com>
2020-12-06 10:27:33 -0500

    example projects added

---
+++ redux/react-redux-example-project/src/reducers/tasksReducer.js
@@ -0,0 +1,28 @@
+import { CREATE_TASK, DELETE_TASK, RESET_TASK_LIST } from '../actions/taskActions';
+
+const tasksReducer = (state = {}, action) => {
+  Object.freeze(state);
+  const nextState = { ...state };
+  switch (action.type) {
+    case CREATE_TASK:
+      // debugger;
+      const newTask = {
+        id: action.taskId,
+        message: action.taskMessage,
+      };
+      nextState[action.taskId] = newTask;
+      return nextState;
+
+    case DELETE_TASK:
+      delete nextState[action.taskId];
+      return nextState;
+
+    case RESET_TASK_LIST:
+      return {};
+
+    default:
+      return state;
+  }
+};
+
+export default tasksReducer;
